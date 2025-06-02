aee1db3dd99efa1f014b99f5f412953a5acc6b05
C14-MohmadZiad <mohmmadziad.ali@gmail.com>
2025-06-02 19:07:05 +0300

    starter code

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// API Routes
+
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
