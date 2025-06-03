76adf0f59fa45160804b9de5606bc9e4c9d96311
LaithTanirah <laith.tannirah.work@gmail.com>
2025-06-03 20:59:01 +0300

    product

--- backend/src/models/connectDB.ts
+++ backend/src/models/connectDB.ts
@@ -3,5 +3,13 @@ import { Pool } from "pg";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+pool
+  .connect()
+  .then(() => {
+    console.log("Db Connected");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
 
 export default pool;
